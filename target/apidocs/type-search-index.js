a45e73ef5f80573d00f65e5c6521e5dda5a09dee
Danuja Dilanka <27912184+danuja-dilanka@users.noreply.github.com>
2026-03-29 23:29:40 +0530

    v1.0.0

--- target/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"globalcloudmedia.fitsms","l":"FitSMS"},{"p":"globalcloudmedia.fitsms","l":"SmsPayload"}];updateSearchResults();
